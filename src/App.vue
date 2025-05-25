479bbfd573d60882eba9572cf365b2d2e82826b8
GaoNeng-wWw <gaonenggg@outlook.com>
2025-05-25 10:34:53 +0800

    eslint + unocss

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-full h-full">
+    <!--  -->
+  </div>
+</template>
+
+<style>
+html,body,#app {
+  width: 100vw;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
+}
+* {
+  box-sizing: border-box;
+}
+</style>
